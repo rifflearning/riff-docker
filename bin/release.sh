cf92e2a136e372908de51072444ebc58b384ab69
Mike Lippert <mike@rifflearning.com>
2020-03-04 16:11:22 -0500

    current (old) emeritus riffrtc and riffdata versions require node ver 10 not 12

--- bin/release.sh
+++ bin/release.sh
@@ -100,7 +100,7 @@ BuildImage()
         riffdata)
             CONTEXT="https://${TOKEN}:@github.com/rifflearning/riff-server.git#${REF}"
             DOCKERFILE="Dockerfile"
-            BUILD_ARGS=( [NODE_VER]=12
+            BUILD_ARGS=( [NODE_VER]=10
                          [PORT]=3000
                        )
             ;;
@@ -108,13 +108,13 @@ BuildImage()
             CONTEXT="https://${TOKEN}:@github.com/rifflearning/riff-rtc.git#${REF}"
             DOCKERFILE="Dockerfile"
             BUILD_STAGE=build
-            BUILD_ARGS=( [NODE_VER]=12
+            BUILD_ARGS=( [NODE_VER]=10
                        )
             ;;
         riffrtc-server)
             CONTEXT="https://${TOKEN}:@github.com/rifflearning/riff-rtc.git#${REF}"
             DOCKERFILE="docker/Dockerfile-prod-server"
-            BUILD_ARGS=( [NODE_VER]=12
+            BUILD_ARGS=( [NODE_VER]=10
                          [RTC_BUILD_TAG]=${TAG}
                          [PORT]=3001
                        )
