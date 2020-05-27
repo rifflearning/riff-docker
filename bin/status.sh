258121668636819d3671a60368b5da65b797b44f
Mike Lippert <mike@riffanalytics.ai>
2020-05-27 14:51:16 -0400

    improve basic status script by reporting on multiple stacks

--- bin/status.sh
+++ bin/status.sh
@@ -10,11 +10,19 @@ echo "--- docker ---"
 echo "- - - IMAGES - - -"
 docker images
 
-echo
-echo "- - - STACK (pfm-stk) Running Processes - - -"
 FORMAT="table {{.Name}}\t{{.CurrentState}}\t{{.Image}}"
 FILTER="desired-state=running"
-docker stack ps --format "$FORMAT" --filter="$FILTER" pfm-stk
+STACKS=( support-stack ssl-stack pfm-stk )
+for stk in "${STACKS[@]}"
+do
+    # Only report on stack processes if some are found
+    if docker stack ps --filter="$FILTER" $stk > /dev/null 2>&1
+    then
+        echo
+        echo "- - - STACK ($stk) Running Processes - - -"
+        docker stack ps --filter="$FILTER" --format "$FORMAT" $stk
+    fi
+done
 
 # TODO:
 # It might be useful to see if there are any shutdown tasks without a later running task
@@ -22,3 +30,28 @@ docker stack ps --format "$FORMAT" --filter="$FILTER" pfm-stk
 # how to produce useful information including the Error
 # FILTER="desired-state=shutdown"
 # FORMAT="table {{.Name}}\t{{.CurrentState}} ({{.Error}})\t{{.Image}}"
+#
+# 1 possible error condition is missing node constraints where the docker inspect of the
+# task looks like:
+#
+# $ docker stack ps support-stack
+# ID                  NAME                         IMAGE                             NODE                DESIRED STATE       CURRENT STATE            ERROR                              PORTS
+# x6atvbxc0lum        support-stack_visualizer.1   dockersamples/visualizer:stable   ip-172-31-13-66     Running             Running 5 seconds ago
+# g8qzz94t4k6w        support-stack_registry.1     registry:2                                            Running             Pending 11 seconds ago   "no suitable node (scheduling …"
+#
+# $ docker inspect g8qzz94t4k6w
+# [
+#     {
+#         "ID": "g8qzz94t4k6wu3koidkrvwtpe",
+#         ...
+#         "Status": {
+#             "Timestamp": "2020-05-27T16:58:56.306225927Z",
+#             "State": "pending",
+#             "Message": "pending task scheduling",
+#             "Err": "no suitable node (scheduling constraints not satisfied on 1 node)",
+#             "PortStatus": {}
+#         },
+#         "DesiredState": "running",
+#         ...
+#     }
+# ]
